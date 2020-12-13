d78a01ebd5b88e9bf6a7833735f8ae630396d2a0
thinhntr <thinhntr@gmail.com>
2020-12-13 12:17:34 +0700

    Remove build  folder

--- target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.BUILD_SETTINGS = {'app_name': 'DocumentScanner', 'author': 'Thinh, Son', 'version': '0.0.0', 'environment': 'local', 'categories': 'Utility;', 'description': '', 'author_email': '', 'url': ''}
